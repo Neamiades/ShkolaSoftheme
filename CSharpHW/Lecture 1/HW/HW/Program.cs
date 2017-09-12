2fb41d3f73442aa595a1ae7c22c1cc56f9688bdc
Яхин Сергей <Neamiades@gmail.com>
2017-09-12 11:56:51 +0300

    Init HW sln; Make task №1.

---
+++ CSharpHW/Lecture 1/HW/HW/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using static System.Console;
+
+namespace HW
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ForegroundColor = ConsoleColor.Yellow;
+            WriteLine("Hello World! My name is Serhii Yakhin.");
+        }
+    }
+}
