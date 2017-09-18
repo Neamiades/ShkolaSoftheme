70bc8a5ae138e47a785d07e9e3f753823ffc9d37
Serhii Yakhin <yahinserhii.l@gmail.com>
2017-09-18 22:30:10 +0300

    Make Lecture 8 task

---
+++ CSharpHW/Lec8_Values_and_references/Task/ConsoleCarCTORApp/Program.cs
@@ -0,0 +1,61 @@
+﻿using static System.Console;
+
+namespace ConsoleCarCTORApp
+{
+    class Engine
+    {
+        public string Type  { get; set; }
+        public string Brand { get; set; }
+    }
+
+
+    class Color
+    {
+        public string Name { get; set; }
+    }
+
+    class Transmission
+    {
+        public string Type { get; set; }
+    }
+
+    class Car
+    {
+        public Engine       Engine       { get; set; }
+        public Color        Color        { get; set; }
+        public Transmission Transmission { get; set; }
+
+        public void PrintInfo()
+        {
+            WriteLine("The car have:");
+            WriteLine($"Engine: {Engine.Brand} {Engine.Type}");
+            WriteLine($"Transmission: {Transmission.Type}");
+            WriteLine($"Color: {Color.Name}");
+        }
+    }
+
+    static class CarConstructor
+    {
+        private static readonly Engine _bestEngine = new Engine { Type = "Electro", Brand = "BMV" };
+
+        public static Car Construct(Engine e, Color c, Transmission t) => new Car {Engine = e, Color = c, Transmission = t};
+        public static Car Reconstruct(Car car, Engine engine = null)
+        {
+            car.Engine = engine ?? _bestEngine;
+            return car;
+        }
+    }
+    class Program
+    {
+        static void Main()
+        {
+            var car = CarConstructor.Construct(
+                new Engine{Type = "Gasoline", Brand = "Audi"},
+                new Color{Name = "Black"},
+                new Transmission{Type = "Mechanic"});
+            car.PrintInfo();
+            CarConstructor.Reconstruct(car);
+            car.PrintInfo();
+        }
+    }
+}
---
+++ CSharpHW/Lec8_Values_and_references/Task/ConsoleCopyApp/Program.cs
@@ -0,0 +1,87 @@
+﻿using static System.Console;
+namespace ConsoleCopyApp
+{
+    class RefUser
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Nickname { get; set; }
+
+        public RefUser GetUserCopy()
+        {
+            var user = new RefUser
+            {
+                FirstName = FirstName,
+                LastName = LastName,
+                Nickname = Nickname
+            };
+
+            return user;
+        }
+
+        public void TellAbout(string paramName)
+        {
+            WriteLine($"User info of {paramName}: (Name: {FirstName} {LastName}, Nickname: {Nickname})");
+            WriteLine("");
+        }
+    }
+
+    struct ValUser
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Nickname { get; set; }
+
+        public void TellAbout(string paramName)
+        {
+            WriteLine($"User info of {paramName}: (Name: {FirstName} {LastName}, Nickname: {Nickname})");
+            WriteLine("");
+        }
+    }
+
+    class Program
+    {
+        static void Main()
+        {
+            var rUser1 = new RefUser { FirstName = "Hex", LastName = "Gaga", Nickname = "Nexo" };
+            var rUser2 = rUser1.GetUserCopy();
+
+            WriteLine("---Stage ONE - referense type users---\n");
+            WriteLine($"---{nameof(rUser2)} is a deep copy of {nameof(rUser1)}---\n");
+            rUser1.TellAbout(nameof(rUser1));
+            rUser2.TellAbout(nameof(rUser2));
+
+            WriteLine($"---Change {nameof(rUser1)} nickname---\n");
+            rUser1.Nickname = "Fox";
+
+            rUser1.TellAbout(nameof(rUser1));
+            rUser2.TellAbout(nameof(rUser2));
+
+            rUser2 = rUser1;
+            WriteLine($"---{nameof(rUser2)} is a shallow copy of {nameof(rUser1)} now---\n");
+
+            rUser1.TellAbout(nameof(rUser1));
+            rUser2.TellAbout(nameof(rUser2));
+
+            WriteLine($"---Change {nameof(rUser2)} nickname---\n");
+            rUser2.Nickname = "Turtle";
+
+            rUser1.TellAbout(nameof(rUser1));
+            rUser2.TellAbout(nameof(rUser2));
+
+            var vUser1 = new ValUser { FirstName = "Jessica", LastName = "White", Nickname = "Gias" };
+            var vUser2 = vUser1;
+
+            WriteLine("---Stage TWO - value type users---\n");
+            WriteLine($"---{nameof(vUser2)} is a deep copy of {nameof(vUser1)}, becouse they are value types---\n");
+            vUser1.TellAbout(nameof(vUser1));
+            vUser2.TellAbout(nameof(vUser2));
+
+            WriteLine($"---Change {nameof(vUser1)} nickname---\n");
+            vUser1.Nickname = "Fox";
+
+            vUser1.TellAbout(nameof(vUser1));
+            vUser2.TellAbout(nameof(vUser2));
+        }
+    }
+}
