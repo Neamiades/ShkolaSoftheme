26fc9c9150015cbd1822f214e8cf625ec1d0fdbb
Serhii Yakhin <Neamiades@gmail.com>
2017-09-19 15:19:32 +0300

    Make Angular Lec2 task

---
+++ AngularHW/Lec2/Task/src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms'; // <-- NgModel lives here
+
+import { AppComponent } from './app.component';
+import { ArticleDetailsComponent } from './article-details/article-details.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ArticleDetailsComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule // <-- import the FormsModule before binding with [(ngModel)]
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
