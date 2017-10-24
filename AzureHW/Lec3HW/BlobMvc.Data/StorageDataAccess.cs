4a0300f65b9b5bda98000c2d59216985c34d1764
Serhii Yakhin <yahinserhii.l@gmail.com>
2017-10-24 03:06:10 +0300

    Azure HW3 alpha version add

---
+++ AzureHW/Lec3HW/BlobMvc.Data/StorageDataAccess.cs
@@ -0,0 +1,225 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using System.Collections.Concurrent;
+using System.Linq;
+using Microsoft.Azure;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Blob;
+using Microsoft.WindowsAzure.Storage.RetryPolicies;
+
+namespace BlobMvc.Data
+{
+    public static class StorageDataAccess
+    {
+        private const string ConnectionStringSettingName = "StorageConnectionString";
+        public  const string ContainerName               = "test";
+
+        //5 * 1024 * 1024 bytes or 5 MB
+        private const int BlockSize = 5_242_880;
+
+        private static readonly string ConnectionString = CloudConfigurationManager.GetSetting(ConnectionStringSettingName);
+        private static readonly CloudBlobContainer CloudBlobContainer = GetContainerReference();
+
+        #region Upload & Download
+
+        public static void Upload(byte[] file, string fileName) => UploadFileInBlocks(file, fileName);
+
+        public static void SimpleUpload(byte[] file, string fileName)
+        {
+            CloudBlockBlob blockBlob = CloudBlobContainer.GetBlockBlobReference(fileName);
+            blockBlob.UploadFromByteArray(file, 0, file.Length);
+        }
+
+        public static void SimpleParallelUpload(byte[] file, string fileName)
+        {
+            CloudBlockBlob blob = CloudBlobContainer.GetBlockBlobReference(Path.GetFileName(fileName));
+
+            BlobRequestOptions requestOptions = new BlobRequestOptions
+            {
+                SingleBlobUploadThresholdInBytes = BlockSize * 2,
+                ParallelOperationThreadCount = 2
+            };
+            
+            blob.StreamWriteSizeInBytes = BlockSize;
+            
+            blob.UploadFromByteArray(file, 0, file.Length, null, requestOptions);
+        }
+
+        public static void ParallelUpload(byte[] file, string fileName, int blockSize = BlockSize)
+        {
+            throw new NotImplementedException();
+            //CloudBlockBlob blob = CloudBlobContainer.GetBlockBlobReference(Path.GetFileName(fileName));
+            
+            //blob.DeleteIfExists();
+            
+            //var blockIDs = new ConcurrentBag<string>();
+            //var stack = new ConcurrentStack<int>();
+
+            //int fileSize = file.Length;
+            //int tasksCount = (int)Math.Ceiling((double) fileSize / blockSize);
+            //for (int i = tasksCount - 1; i >= 0; i--)
+            //{
+            //    stack.Push(i);
+            //}
+            //var tasks = new Task[tasksCount];
+            //for (int i = 0; i < tasksCount; i++)
+            //{
+            //    tasks[i] = Task.Factory.StartNew(() =>
+            //    {
+            //        stack.TryPop(out int blockId);
+            //        int byteIdx = blockId * blockSize;
+            //        int blockLength = Math.Min(blockSize, fileSize - byteIdx);
+
+
+            //        byte[] bytesToUpload = new byte[blockLength];
+            //        Array.Copy(file, byteIdx, bytesToUpload, 0, blockLength);
+
+            //        var blockIdEncoded = Convert.ToBase64String(BitConverter.GetBytes((int)blockId));
+
+            //        using (MemoryStream memoryStream = new MemoryStream(bytesToUpload, 0, blockLength))
+            //        {
+            //            lock (blob)
+            //            {
+            //                blockIDs.Add(blockIdEncoded);
+            //                blob.PutBlock(blockIdEncoded, memoryStream, null, null, new BlobRequestOptions
+            //                {
+            //                    RetryPolicy = new ExponentialRetry(TimeSpan.FromSeconds(2), 1)
+            //                });
+            //            }
+            //        }
+
+            //    });
+            //}
+            //Task.WaitAll(tasks);
+            //Parallel.ForEach(stack, blockId => 
+            //{
+            //    int byteIdx = blockId * blockSize;
+            //    int blockLength = Math.Min(blockSize, fileSize - byteIdx);
+
+            //    string blockIdEncoded = GetBase64BlockId(blockId);
+
+            //    byte[] bytesToUpload = new byte[blockLength];
+            //    Array.Copy(file, byteIdx, bytesToUpload, 0, blockLength);
+
+            //    using (MemoryStream memoryStream = new MemoryStream(bytesToUpload, 0, blockLength))
+            //    {
+            //        lock (blob)
+            //        {
+            //            blockIDs.Add(blockIdEncoded);
+            //            blob.PutBlock(blockIdEncoded, memoryStream, null, null, new BlobRequestOptions
+            //            {
+            //                RetryPolicy = new ExponentialRetry(TimeSpan.FromSeconds(2), 1)
+            //            });
+            //        }
+            //    }
+            //});
+
+            blob.PutBlockList(blockIDs);
+        }
+
+        public static byte[] DownloadFile(string fileName)
+        {
+            CloudBlockBlob blob = CloudBlobContainer.GetBlockBlobReference(Path.GetFileName(fileName));
+
+            blob.FetchAttributes();
+            long fileSize = blob.Properties.Length;
+
+            byte[] blobContents = new byte[fileSize];
+            int position = 0;
+
+            while (fileSize > 0)
+            {
+                int blockLength = (int)Math.Min(BlockSize, fileSize);
+
+                blob.DownloadRangeToByteArray(blobContents, position, position, blockLength);
+
+                position += blockLength;
+                fileSize -= BlockSize;
+            }
+
+            return blobContents;
+        }
+
+        public static byte[] DownloadFileParallel(string fileName, int blockSize = BlockSize)
+        {
+            CloudBlockBlob blob = CloudBlobContainer.GetBlockBlobReference(Path.GetFileName(fileName));
+
+            blob.FetchAttributes();
+            long fileSize = blob.Properties.Length;
+            byte[] blobContents = new byte[fileSize];
+            int position = 0;
+
+            int tasksCount = (int)Math.Ceiling((double)fileSize / blockSize);
+            var tasks = new Task[tasksCount];
+            for (int i = 0; i < tasksCount; i++, position += blockSize)
+            {
+                tasks[i] = Task.Factory.StartNew((pos) =>
+                {
+                    int blockLength = (int)Math.Min(blockSize, fileSize - (int)pos);
+
+                    blob.DownloadRangeToByteArray(blobContents, (int)pos, (int)pos, blockLength);
+
+                }, position);
+            }
+            Task.WaitAll(tasks);
+
+            return blobContents;
+        }
+
+        private static void UploadFileInBlocks(byte[] file, string fileName)
+        {
+            CloudBlockBlob blob = CloudBlobContainer.GetBlockBlobReference(Path.GetFileName(fileName));
+
+            blob.DeleteIfExists();
+
+            List<string> blockIDs = new List<string>();
+            long fileSize = file.Length;
+
+            int blockId = 0;
+
+            while (fileSize > 0)
+            {
+                int blockLength = (int)Math.Min(BlockSize, fileSize);
+
+                string blockIdEncoded = GetBase64BlockId(blockId);
+                blockIDs.Add(blockIdEncoded);
+
+                byte[] bytesToUpload = new byte[blockLength];
+                Array.Copy(file, blockId * BlockSize, bytesToUpload, 0, blockLength);
+
+                using (MemoryStream memoryStream = new MemoryStream(bytesToUpload, 0, blockLength))
+                {
+                    blob.PutBlock(blockIdEncoded, memoryStream, null, null, new BlobRequestOptions
+                    {
+                        RetryPolicy = new ExponentialRetry(TimeSpan.FromSeconds(2), 1)
+                    });
+                }
+
+                blockId++;
+                fileSize -= blockLength;
+            }
+
+            blob.PutBlockList(blockIDs);
+        }
+
+        #endregion
+
+        public static List<IListBlobItem> ListBlobs() => CloudBlobContainer.ListBlobs(null, true).ToList();
+
+        private static string GetBase64BlockId(int blockId) => Convert.ToBase64String(Encoding.ASCII.GetBytes($"{blockId:0000000}"));
+
+        private static CloudBlobContainer GetContainerReference()
+        {
+            CloudStorageAccount storageAccount = CloudStorageAccount.Parse(ConnectionString);
+            CloudBlobClient blobClient = storageAccount.CreateCloudBlobClient();
+            CloudBlobContainer container = blobClient.GetContainerReference(ContainerName);
+
+            container.CreateIfNotExists();
+
+            return container;
+        }
+    }
+}
--- AzureHW/Lec3HW/BlobMvc/Controllers/HomeController.cs
+++ AzureHW/Lec3HW/BlobMvc/Controllers/HomeController.cs
@@ -1,30 +1,103 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Microsoft.WindowsAzure.Storage.Blob;
+using BlobMvc.Data;
 
 namespace BlobMvc.Controllers
 {
     public class HomeController : Controller
     {
+        [HttpGet]
         public ActionResult Index()
         {
+            ViewBag.Container = StorageDataAccess.ContainerName;
             return View();
         }
 
-        public ActionResult About()
+        [HttpPost]
+        public ActionResult Index(string uploadType, HttpPostedFileBase file)
         {
-            ViewBag.Message = "Your application description page.";
+            if (file != null)
+            {
+                byte[] avatar = new byte[file.ContentLength];
+                file.InputStream.Read(avatar, 0, file.ContentLength);
 
-            return View();
+                string fileName = System.IO.Path.GetFileName(file.FileName);
+                if (uploadType.Equals("simple"))
+                {
+                    StorageDataAccess.Upload(avatar, $"Simple{fileName}");
+                }
+                else
+                {
+                    StorageDataAccess.ParallelUpload(avatar, $"Parallel{fileName}");
+                }
+            }
+
+            return PartialView("_Blobs", StorageDataAccess.ListBlobs().Select(b => (CloudBlockBlob)b));
         }
 
-        public ActionResult Contact()
+        [HttpGet]
+        public ActionResult GetBlobList()
         {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
+            return PartialView("_Blobs", StorageDataAccess.ListBlobs().Select(b => (CloudBlockBlob)b));
         }
+
+        [HttpGet]
+        public FileResult Download(string blobName)
+        {
+            byte[] fileBytes = StorageDataAccess.DownloadFile(blobName);
+            return File(fileBytes, System.Net.Mime.MediaTypeNames.Application.Octet, blobName);
+        }
+
+        [HttpGet]
+        public FileResult DownloadInParallel(string blobName)
+        {
+            byte[] fileBytes = StorageDataAccess.DownloadFileParallel(blobName);
+            return File(fileBytes, System.Net.Mime.MediaTypeNames.Application.Octet, blobName);
+        }
+
+        //[HttpPost]
+        //public ActionResult Upload(HttpPostedFileBase file)
+        //{
+        //    if (file != null)
+        //    {
+        //        byte[] avatar = new byte[file.ContentLength];
+        //        file.InputStream.Read(avatar, 0, file.ContentLength);
+
+        //        string fileName = System.IO.Path.GetFileName(file.FileName);
+        //        StorageDataAccess.ParallelUpload(avatar, fileName);
+        //    }
+
+        //    return PartialView("_Blobs", StorageDataAccess.ListBlobs().Select(b => (CloudBlockBlob)b));
+        //}
+
+        [HttpPost]
+        public ActionResult AjaxUpload(HttpPostedFileBase file)
+        {
+            if (file != null)
+            {
+                byte[] avatar = new byte[file.ContentLength];
+                file.InputStream.Read(avatar, 0, file.ContentLength);
+
+                string fileName = System.IO.Path.GetFileName(file.FileName);
+                StorageDataAccess.Upload(avatar, fileName);
+                return Json(new { success = true, message = $"{fileName} is succefuly uploaded" });
+            }
+
+            return Json(new { success = false, message = "Error while uploading file" });
+        }
+
+        //public FileResult Upload(string blobName)
+        //{
+        //    byte[] fileBytes = StorageDataAccess.DownloadFileInBlocks(blobName);
+        //    return File(fileBytes, System.Net.Mime.MediaTypeNames.Application.Octet, blobName);
+        //}
+
+        //public FileResult UploadInParallel(string blobName)
+        //{
+        //    byte[] fileBytes = StorageDataAccess.DownloadFileInBlocksParallel(blobName);
+        //    return File(fileBytes, System.Net.Mime.MediaTypeNames.Application.Octet, blobName);
+        //}
     }
 }
